9d2a0bae86bb4c966bf778272d17cf92d420ba17
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-10 22:10:56 -0500

    Manager tests pass

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,13 +4,21 @@ const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
-
+    const office = 20;
+    const employ = new Manager("Susan", 15, "abc123@gmail.com", office);
+    expect(employ.officeNumber).toBe(office);
 });
 
 test('getRole() should return "Manager"', () => {
+    const role = "Manager";
+    const employ = new Manager("Susan", 15, "abc123@gmail.com", 20);
+    expect(employ.getRole()).toBe(role);
 
 });
 
 test("Can get office number via getOffice()", () => {
 
+    const office = 20;
+    const employ = new Manager("Susan", 15, "abc123@gmail.com", office);
+    expect(employ.getOfficeNumber()).toBe(office);
 });
