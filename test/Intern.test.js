4087af91e43d9d44cec435460971926ed28394f9
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-10 22:05:50 -0500

    Intern tests pass

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -3,13 +3,21 @@
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
+    const school = "UNCSA";
+    const employ = new Intern("Susan", 15, "abc123@gmail.com", school);
+    expect(employ.school).toBe(school);
 
 });
 
 test("getRole() should return \"Intern\"", () => {
-
+    const role = "Intern";
+    const employ = new Intern("Susan", 15, "abc123@gmail.com", "UNCSA");
+    expect(employ.getRole()).toBe(role);
 });
 
 test("Can get school via getSchool()", () => {
+    const school = "UNCSA";
+    const employ = new Intern("Susan", 15, "abc123@gmail.com", school);
+    expect(employ.getSchool()).toBe(school);
 
 });
