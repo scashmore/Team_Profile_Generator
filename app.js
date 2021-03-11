4fc14468968090d691da473e05aba9d3721d2597
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-10 23:43:16 -0500

    inquiered/app.js overhaul

--- app.js
+++ app.js
@@ -11,111 +11,225 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { createReadStream } = require("node:fs");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
 const team = [];
 
-function init() {
-    mainHtml();
-    addTeamMembers();
-}
+function getManager() {
+    inquirer.prompt([
+        {
+            message: "Enter the manager's name",
+            name: "name"
+        },
+
+        {
+            message: "Enter the manager's id",
+            name: "id"
+        },
+
+        {
+            message: "Enter the manager's email",
+            name: "email"
+        },
+
+        {
+            message: "Enter the manager's office number",
+            name: "officeNumber"
+        }
+    ])
+        .then(function managerPush(responce) {
+            const newManager = new Manager(responce.name, responce.id, responce.email, responce.officeNumber);
+            team.push(newManager);
+            addTeamMembers();
+        });
+};
 
 function addTeamMembers() {
-    inquirer.prompt(
-        [
-            {
-                message: "Enter the team manager's name",
-                name: "name"
-            },
-
-            {
-                message: "Enter the manager's id",
-                name: "id"
-            },
-
-            {
-                message: "Enter the manager's email",
-                name: "email"
-            },
-
-            {
-                message: "Enter the manager's office number",
-                name: "officeNumber"
+    inquirer.prompt([{
+        message: "Which type of team member would you like to add?",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add more team members"
+        ],
+        name: "choice"
+    }
+    ])
+        .then(function choice(chosen) {
+            switch (chosen.choices) {
+                case "Engineer":
+                    addEngin();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                case "I don't want to add more team members":
+                    makeTeam();
+                    break;
             }
-        ]
-    )
-    inquirer.prompt(
-        [
-            {
-                message: "Which tyle of team member would you like to add?",
-                choices: [
-                    "Engineer",
-                    "Intern",
-                    "I don't want to add more team members"
-                ],
-                name: "role"
-            }
-        ]
-    )
-    .then(function({role}) {
-        if (role === "Engineer") {
-            inquirer.prompt(
-                [
-                    {
-                        message: "Enter the engineer's name",
-                        name: "name"
-                    },
-        
-                    {
-                        message: "Enter the engineer's id",
-                        name: "id"
-                    },
-        
-                    {
-                        message: "Enter the engineer's email",
-                        name: "email"
-                    },
-        
-                    {
-                        message: "Enter the engineer's GitHub",
-                        name: "github"
-                    }
-                ]
-            )
-        }
-        else if (role === "Intern") {
-            inquirer.prompt(
-                [
-                    {
-                        message: "Enter the intern's name",
-                        name: "name"
-                    },
-        
-                    {
-                        message: "Enter the intern's id",
-                        name: "id"
-                    },
-        
-                    {
-                        message: "Enter the intern's email",
-                        name: "email"
-                    },
-        
-                    {
-                        message: "Enter the intern's school",
-                        name: "school"
-                    }
-                ]
-            )
-        }
-        else {
-            console.log("You have added all your team members!")
-        }
-    })
-}
+        });
+};
+
+function addEngin() {
+    inquirer.prompt([{
+        message: "Enter the engineer's name",
+        name: "name"
+    },
+
+    {
+        message: "Enter the engineer's id",
+        name: "id"
+    },
+
+    {
+        message: "Enter the engineer's email",
+        name: "email"
+    },
+
+    {
+        message: "Enter the engineer's GitHub",
+        name: "github"
+    },
+    ])
+        .then(function enginPush(responce) {
+            const newEngineer = new Engineer(responce.name, responce.id, responce.email, responce.github);
+            team.push(newEngineer);
+            addTeamMembers();
+        });
+};
+
+function addIntern() {
+    inquirer.prompt([{
+        message: "Enter the intern's name",
+        name: "name"
+    },
+
+    {
+        message: "Enter the intern's id",
+        name: "id"
+    },
+
+    {
+        message: "Enter the intern's email",
+        name: "email"
+    },
+
+    {
+        message: "Enter the intern's school",
+        name: "school"
+    }
+    ])
+        .then(function internPush(responce) {
+            const newIntern = new Intern(responce.name, responce.id, responce.email, responce.github);
+            team.push(newIntern);
+            addTeamMembers();
+        });
+};
+
+
+
+// function init() {
+//     mainHtml();
+//     addTeamMembers();
+// }
+
+// function addTeamMembers() {
+//     inquirer.prompt(
+//         [
+//             {
+//                 message: "Enter the team manager's name",
+//                 name: "name"
+//             },
+
+//             {
+//                 message: "Enter the manager's id",
+//                 name: "id"
+//             },
+
+//             {
+//                 message: "Enter the manager's email",
+//                 name: "email"
+//             },
+
+//             {
+//                 message: "Enter the manager's office number",
+//                 name: "officeNumber"
+//             }
+//         ]
+//     )
+//         .then(inquirer.prompt(
+//             [
+//                 {
+//                     message: "Which tyle of team member would you like to add?",
+//                     choices: [
+//                         "Engineer",
+//                         "Intern",
+//                         "I don't want to add more team members"
+//                     ],
+//                     name: "choice"
+//                 }
+//             ]
+//         )
+//             .then(function ({ role }) {
+//                 if (choice === "Engineer") {
+//                     inquirer.prompt(
+//                         [
+//                             {
+//                                 message: "Enter the engineer's name",
+//                                 name: "name"
+//                             },
+
+//                             {
+//                                 message: "Enter the engineer's id",
+//                                 name: "id"
+//                             },
+
+//                             {
+//                                 message: "Enter the engineer's email",
+//                                 name: "email"
+//                             },
+
+//                             {
+//                                 message: "Enter the engineer's GitHub",
+//                                 name: "github"
+//                             }
+//                         ]
+//                     )
+//                 }
+//                 else if (choice === "Intern") {
+//                     inquirer.prompt(
+//                         [
+//                             {
+//                                 message: "Enter the intern's name",
+//                                 name: "name"
+//                             },
+
+//                             {
+//                                 message: "Enter the intern's id",
+//                                 name: "id"
+//                             },
+
+//                             {
+//                                 message: "Enter the intern's email",
+//                                 name: "email"
+//                             },
+
+//                             {
+//                                 message: "Enter the intern's school",
+//                                 name: "school"
+//                             }
+//                         ]
+//                     )
+//                 }
+//                 else {
+//                     console.log("You have added all your team members!")
+//                 }
+//             })
+//         )
+// }
 
 
 // After the user has input all employees desired, call the `render` function (required
