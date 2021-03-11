6db61407158ac118a1b4c6d3feb05c492d836485
Susan Ashmore <scashmorecitcom@gmail.com>
2021-03-10 22:02:30 -0500

    Engineer tests pass

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -3,13 +3,22 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
+    const git = "scagithub";
+    const employ = new Engineer("Susan", 15, "abc123@gmail.com", git);
+    expect(employ.github).toBe(git);
 
 });
 
 test("getRole() should return \"Engineer\"", () => {
+    const role = "Engineer";
+    const employ = new Engineer("Susan", 15, "abc123@gmail.com", "scasgithub");
+    expect(employ.getRole()).toBe(role);
 
 });
 
 test("Can get GitHub username via getGithub()", () => {
+    const git = "scagithub";
+    const employ = new Engineer("Susan", 15, "abc123@gmail.com", git);
+    expect(employ.getGithub()).toBe(git);
 
 });
